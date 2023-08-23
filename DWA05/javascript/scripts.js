547e2a22e2bb7225f16d53a0a901e4259aeb3236
MihlaliKota <hlalikota@gmail.com>
2023-08-23 14:05:46 +0200

    Update

--- DWA05/javascript/scripts.js
+++ DWA05/javascript/scripts.js
@@ -5,16 +5,24 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  if (dividend === "" || divider === "") {
-    result.innerText =
-      "Division not performed. Both values are required in inputs. Try again.";
-  } else if (isNaN(dividend) || isNaN(divider) || parseFloat(divider) < 0) {
-    result.innerText =
-      "Division not performed. Invalid number provided. Try again.";
-    console.error("Invalid division:", dividend, divider);
-  } else {
-    result.innerText = Math.floor(dividend / divider);
+  try {
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+    if (dividend === "" || divider === "") {
+      result.innerText =
+        "Division not performed. Both values are required in inputs. Try again.";
+    } else if (dividend && isNaN(divider) && parseFloat(divider) < 0) {
+      result.innerText =
+        "Division not performed. Invalid number provided. Try again.";
+      console.error("Invalid division:", dividend, divider);
+    } else if (isNaN(dividend) || isNaN(divider)) {
+      document.body.innerHTML =
+        "Something critical went wrong. Please reload the page.";
+      throw new Error("Invalid inputs");
+    } else {
+      result.innerText = Math.floor(dividend / divider);
+    }
+  } catch (e) {
+    console.error(e);
   }
 });
